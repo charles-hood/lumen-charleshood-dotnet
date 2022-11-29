0da19f27184255469ba204588b4b61f328de2d96
Charles Hood <charles.hood@gmail.com>
2022-11-29 10:59:48 -0500

    Update ContactTemplate.tsx

--- src/templates/ContactTemplate/ContactTemplate.tsx
+++ src/templates/ContactTemplate/ContactTemplate.tsx
@@ -24,23 +24,23 @@ const ContactTemplate: React.FC<Props> = ({ data }: Props) => {
     <Layout>
       <Sidebar />
       <Page title={title}>
-	<form class="the-contact-form" name="plzcontactme" method="POST" data-netlify="true">
-	<label for="name">Name:</label>
-	<input type="text" id="name" name="user_name" />
-	<label for="mail">Email:</label>
-	<input type="email" id="mail" name="user_mail" />
-	<label for="note">Message:</label>
-	<textarea id="msg" name="user_message" rows="5" cols="40"></textarea>
-	<button style="font-size: 18px;" type="submit">Send</button>
-	</form>
         <div dangerouslySetInnerHTML={{ __html: body }} />
+        <form class="the-contact-form" name="plzcontactme" method="POST" data-netlify="true">
+	      <label for="name">Name:</label>
+      	<input type="text" id="name" name="user_name" />
+	      <label for="mail">Email:</label>
+	      <input type="email" id="mail" name="user_mail" />
+	      <label for="note">Message:</label>
+	      <textarea id="msg" name="user_message" rows="5" cols="40"></textarea>
+          <p><button style="font-size: 18px;" type="submit">Send</button></p>
+	      </form>
       </Page>
     </Layout>
   );
 };
 
 export const query = graphql`
-  query PageTemplate($slug: String!) {
+  query ContactTemplate($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       id
       html
