fc8be7a6b15fe39df0fb9d39a67e18e73529059d
Charles Hood <charles.hood@gmail.com>
2022-12-06 21:17:28 -0500

    format twitter follow
    at bottom of posts

--- src/components/Post/Author/Author.tsx
+++ src/components/Post/Author/Author.tsx
@@ -10,7 +10,8 @@ const Author = () => {
 
   return (
     <div className={styles.author}>
-      <p style="text-align:center;">
+      <p className={styles.bio}>
+        {author.bio}
 	<a href="https://twitter.com/charleshood" target="_blank" rel="nofollow noopener noreferrer">
 		<img src="https://img.shields.io/twitter/url/https/twitter.com/bukotsunikki.svg?style=social&amp;label=Follow%20%40charleshood" alt="Twitter URL"/>
 	</a>
